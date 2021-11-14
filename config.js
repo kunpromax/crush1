b4351a7f4ee39807fc8725a0b6d0dd6605da9f3c
kunpromax <82646217+kunpromax@users.noreply.github.com>
2021-11-14 12:38:32 +0700

    config

---
+++ config.js
@@ -0,0 +1,18 @@
+const configloi = {
+    tenweb: "Code tỏ tình bằng javascript siêu cấp pro",
+    infokey: "Code tỏ tình bằng javascript siêu cấp pro gửi đến crush phát là có ny liền",
+    introTitle: 'He luu cậu!',
+    introDesc: `Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.`,
+    btnIntro: 'Ok La',
+    title: 'Cậu yêu tớ có phải không nào ._.',
+    desc: 'Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v',
+    btnOk: 'Yêu ơi là yêu <3',
+    btnEo: 'Cậu mơ à',
+    question: 'Nói cho tớ lí do cậu thích tớ đi :vvvv',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Vì bạn đẹp trai & khoai to <3',
+    mess: 'Tớ biết mà ^^ Yêu cậu 300.000',
+    messDesc: 'Tối nay tớ qua đón cậu đi chơi nhaaaaaaaaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàoooooo',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/kunmaxstudio'
+        }
